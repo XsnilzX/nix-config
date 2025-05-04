c372acfccb3e38ea15c107f6757e87a015a24dac
Richard Taesler <richard.hans.taesler@stud.uni-hannover.de>
2025-05-04 14:53:07 +0200

    Update xsnilzx.nix

--- users/xsnilzx.nix
+++ users/xsnilzx.nix
@@ -19,7 +19,12 @@
          # find more options here: https://mozilla.github.io/policy-templates/
      };
     # hyprland stuff
-    waybar rofi-wayland dunst hyprpaper wlogout cliphist
+    waybar
+    rofi-wayland
+    dunst
+    hyprpaper
+    wlogout
+    cliphist
   ];
 
   # Ganze Ordner einbinden (inkl. Scripts, Styles usw.)
