2f9c9f169a4c1fd8d04b2dcd9d93edabdefbc783
Richard Taesler <richard.hans.taesler@stud.uni-hannover.de>
2025-05-04 20:01:46 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration with Hyprland + LUKS for Yoga Pro 7";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
