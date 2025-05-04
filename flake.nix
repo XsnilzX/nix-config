b524dee9cf1fde818eee2346d5aec1e768d81c35
Richard Taesler <richard.hans.taesler@stud.uni-hannover.de>
2025-05-04 16:03:41 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -38,6 +38,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.users.xsnilzx = import ./users/xsnilzx.nix;
           }
 
           # Aktiviert unfreie Software systemweit
